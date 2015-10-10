9c281adc2b79fba7aba9d21df00d8a8054661422
vineverlas <vineverlas@yahoo.com>
2015-10-10 18:27:59 -0500

    Create Best_Time_To_Buy_And_Sell_Stock_II.java

---
+++ Best_Time_To_Buy_And_Sell_Stock_II.java
@@ -0,0 +1,36 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. 
+You may complete as many transactions as you like 
+(ie, buy one and sell one share of the stock multiple times). 
+However, you may not engage in multiple transactions at the same time 
+(ie, you must sell the stock before you buy again).
+*/
+
+public class Solution {
+    
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        
+        for(int i=0;i<prices.length;i++){
+            if(minprice > prices[i]){
+                minprice=prices[i];
+                continue;
+            }
+            
+            if(i+1 != prices.length) {
+                if(prices[i] < prices[i+1]) {
+                    continue;
+                }
+            }
+            
+            maxprofit += prices[i] - minprice;
+            minprice = Integer.MAX_VALUE;
+        }
+        
+        return maxprofit;
+    }
+    
+}
