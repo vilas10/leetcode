476c0dfa4629067c85ab4b24f0c65d82f3b2bc6f
vineverlas <vineverlas@yahoo.com>
2015-10-10 17:42:25 -0500

    Update and rename Summary Ranges to Summary_Ranges.java

--- Summary_Ranges.java
+++ Summary_Ranges.java
@@ -7,7 +7,7 @@ Credits:
 Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 */
 
-public class Solution {
+public class Summary_Ranges {
     public List<String> summaryRanges(int[] nums) {
         
         
