7fe796abff74540623183d66abb6f33c0b6c21a2
vineverlas <vineverlas@yahoo.com>
2015-10-10 18:43:46 -0500

    Create Remove_Element.java

---
+++ Remove_Element.java
@@ -0,0 +1,25 @@
+/*
+Given an array and a value, remove all instances of that value in place and return the new length.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int l = nums.length;
+        
+        for(int i=0;i<l;i++) {
+            if(nums[i] == val) {
+                nums[i] = nums[l-1];
+                nums[l-1] = val-1;
+                l--;
+                i--;
+            }
+        }
+        
+        return l;
+    }
+}
