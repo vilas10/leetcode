595757f7cb0d789d425b949d80368846b510d37a
vineverlas <vineverlas@yahoo.com>
2015-10-10 18:25:50 -0500

    Create Word_Break.java

---
+++ Word_Break.java
@@ -0,0 +1,44 @@
+/*
+Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
+
+Return true because "leetcode" can be segmented as "leet code".
+*/
+
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if(s == null || s.length() == 0) {
+            return false;
+        }
+        
+        boolean[] flag = new boolean[s.length()+1];
+        flag[0] = true;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(flag[i] == false) {
+                continue;
+            }
+            
+            for(String word: wordDict) {
+                int l = word.length();
+                int end = i + l;
+                if(end > s.length()) {
+                    continue;
+                }
+                
+                if(flag[end] == true) {
+                    continue;
+                }
+                
+                if(s.substring(i, end).equals(word)) {
+                    flag[end] = true;
+                }
+            }
+        }
+        
+        return flag[s.length()];
+    }
+}
