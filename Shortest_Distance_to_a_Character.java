6193aae81385a3961c49bbe7db7c93a79d510fdf
vineverlas <vineverlas10@gmail.com>
2018-07-18 14:53:12 -0500

    Create Shortest_Distance_to_a_Character.java

---
+++ Shortest_Distance_to_a_Character.java
@@ -0,0 +1,9 @@
+/*
+Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+*/
+
+class Solution {
+    public int[] shortestToChar(String S, char C) {
+        
+    }
+}
