ac286a4cfd42b8d011d61ad4a51ba86ffeb75810
vineverlas <vineverlas10@gmail.com>
2018-07-18 14:37:14 -0500

    Create Number_compliment.java

---
+++ Number_compliment.java
@@ -0,0 +1,13 @@
+/*
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+Note:
+The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+You could assume no leading zero bit in the integer’s binary representation.
+*/
+
+class Solution {
+    public int findComplement(int num) {
+        
+    }
+}
