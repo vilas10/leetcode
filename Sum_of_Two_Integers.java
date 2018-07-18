6fdca73f28ee729417dc3edfd184a2f050ea9642
vineverlas <vineverlas10@gmail.com>
2018-07-18 16:36:37 -0500

    Create Sum_of_Two_Integers.java

---
+++ Sum_of_Two_Integers.java
@@ -0,0 +1,9 @@
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+*/
+
+class Solution {
+    public int getSum(int a, int b) {
+        
+    }
+}
