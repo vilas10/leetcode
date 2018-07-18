34cf42cc533fb2aa433a9e597da59d855611a47b
vineverlas <vineverlas10@gmail.com>
2018-07-18 15:40:25 -0500

    Create Binary_Number_with_Alternating_Bits.java

---
+++ Binary_Number_with_Alternating_Bits.java
@@ -0,0 +1,9 @@
+/*
+Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+*/
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        
+    }
+}
