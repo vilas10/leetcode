ab4693e8996dcac089db191f7983037a4977bf04
vineverlas <vineverlas10@gmail.com>
2018-07-18 14:35:18 -0500

    Create Binary_Gap.java

---
+++ Binary_Gap.java
@@ -0,0 +1,11 @@
+/*
+Given a positive integer N, find and return the longest distance between two consecutive 1's in the binary representation of N.
+
+If there aren't two consecutive 1's, return 0.
+*/
+
+class Solution {
+    public int binaryGap(int N) {
+        
+    }
+}
