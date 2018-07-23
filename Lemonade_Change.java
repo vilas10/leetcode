2dc307623e6fbc4b13a812c1a9fc7f84c37e772b
vineverlas <vineverlas10@gmail.com>
2018-07-23 15:47:02 -0500

    Create Lemonade_Change.java

---
+++ Lemonade_Change.java
@@ -0,0 +1,32 @@
+/*
+At a lemonade stand, each lemonade costs $5.
+Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+Note that you don't have any change in hand at first.
+Return true if and only if you can provide every customer with correct change.
+*/
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill: bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
