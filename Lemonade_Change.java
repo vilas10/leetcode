c44ffe790c4320d22f2362e30cf85e7321c10737
vineverlas <vineverlas10@gmail.com>
2018-07-23 15:52:04 -0500

    Update Lemonade_Change.java

--- Lemonade_Change.java
+++ Lemonade_Change.java
@@ -7,26 +7,31 @@ Return true if and only if you can provide every customer with correct change.
 */
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int five = 0, ten = 0;
-        for (int bill: bills) {
-            if (bill == 5)
-                five++;
-            else if (bill == 10) {
-                if (five == 0) return false;
-                five--;
-                ten++;
-            } else {
-                if (five > 0 && ten > 0) {
-                    five--;
-                    ten--;
-                } else if (five >= 3) {
-                    five -= 3;
-                } else {
+        if (bills.length == 0) return true;
+        if (bills[0] != 5) return false;
+        int fives = 0;
+        int tens = 0;
+        
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                fives++;
+            } else if (bills[i] == 10) {
+                if (fives == 0) return false;
+                fives--;
+                tens++;
+            } else if (bills[i] == 20) {
+                if (fives == 0) return false;
+                if (tens > 0) {
+                    tens--;
+                    fives--;
+                } else if (fives < 3) {
                     return false;
+                } else {
+                    fives -= 3;
                 }
             }
         }
-
+        
         return true;
     }
 }
