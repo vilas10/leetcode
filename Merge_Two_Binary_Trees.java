ec62abfb449748ae3b84172fe36320107eb7b892
vineverlas <vineverlas10@gmail.com>
2018-07-25 16:54:56 -0500

    Update Merge_Two_Binary_Trees.java

--- Merge_Two_Binary_Trees.java
+++ Merge_Two_Binary_Trees.java
@@ -13,8 +13,17 @@ You need to merge them into a new binary tree. The merge rule is that if two nod
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+public class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        else if (t1 == null) return t2;
+        else if (t2 == null) return t1;
         
+        TreeNode t12 = new TreeNode(t1.val + t2.val);
+        
+        t12.left = mergeTrees(t1.left, t2.left);
+        t12.right = mergeTrees(t1.right, t2.right);
+        
+        return t12;
     }
 }
