d3ac69441710ac5d8e01a66018b130de487be6c7
vineverlas <vineverlas10@gmail.com>
2018-07-25 15:59:52 -0500

    Update Average_of_Levels_in_Binary_Tree.java

--- Average_of_Levels_in_Binary_Tree.java
+++ Average_of_Levels_in_Binary_Tree.java
@@ -13,6 +13,30 @@ Given a non-empty binary tree, return the average value of the nodes on each lev
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         
+        if (root == null) return result;
+        queue.add(root);
+        
+        while(!queue.isEmpty()) {
+            int counter = queue.size();
+            int n = counter;
+            double sum = 0;
+            
+            while(counter > 0) {
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+                
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+                
+                counter--;
+            }
+            
+            result.add(new Double(sum/n));
+        }
+        
+        return result;
     }
 }
