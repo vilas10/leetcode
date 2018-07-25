8b10c252172985ed2ce3ceea260c47cce4cbb364
vineverlas <vineverlas10@gmail.com>
2018-07-25 16:38:57 -0500

    Update Find All Numbers Disappeared_In_An_Array.java

--- Find All Numbers Disappeared_In_An_Array.java
+++ Find All Numbers Disappeared_In_An_Array.java
@@ -5,9 +5,28 @@ Find all the elements of [1, n] inclusive that do not appear in this array.
 
 Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
 */
-
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
         
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0 && nums[i] != (i+1)) {
+                int temp = nums[i];
+                
+                if (nums[i] != nums[temp - 1]) {
+                    nums[i] = nums[temp - 1];
+                    nums[temp - 1] = temp;
+                    i--;
+                } else {
+                    nums[i] = 0;
+                }
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) result.add(i + 1);
+        }
+        
+        return result;
     }
 }
