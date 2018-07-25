c78c06e1390f6f31aa3bb706dd9a08dedcb59765
vineverlas <vineverlas10@gmail.com>
2018-07-25 16:14:19 -0500

    Update Binary_Gap.java

--- Binary_Gap.java
+++ Binary_Gap.java
@@ -6,6 +6,26 @@ If there aren't two consecutive 1's, return 0.
 
 class Solution {
     public int binaryGap(int N) {
+        int gap = 0;
+        int maxgap = 0;
+        boolean found = false;
+        while (N > 0) {
+            int bit = (N & 1);
+            
+            if (bit == 1) {
+                if (found) {
+                    gap++;
+                    maxgap = Math.max(gap, maxgap);
+                }
+                found = true;
+                gap = 0;
+            } else if (found) {
+                gap++;
+            }
+            
+            N = N >> 1;
+        }
         
+        return maxgap;
     }
 }
