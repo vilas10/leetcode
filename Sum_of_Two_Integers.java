45a7b9d5dcfa771b3c4f79c28a56f4e02a6aa275
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:53:53 -0500

    Update Sum_of_Two_Integers.java

--- Sum_of_Two_Integers.java
+++ Sum_of_Two_Integers.java
@@ -4,6 +4,12 @@ Calculate the sum of two integers a and b, but you are not allowed to use the op
 
 class Solution {
     public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
         
+        return a;
     }
 }
