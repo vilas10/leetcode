2b3209c8a18ab3a3111b1e16060aec2f1adb32c2
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:55:11 -0500

    Update Transpose_Matrix.java

--- Transpose_Matrix.java
+++ Transpose_Matrix.java
@@ -7,6 +7,14 @@ The transpose of a matrix is the matrix flipped over it's main diagonal, switchi
 
 class Solution {
     public int[][] transpose(int[][] A) {
+        int[][] trans = new int[A[0].length][A.length];
         
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                trans[j][i] = A[i][j];
+            }
+        }
+        
+        return trans;
     }
 }
