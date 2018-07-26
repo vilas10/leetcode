be0fda4a38b8c5a9bc84a47ddb0db37a269f5ce4
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:46:15 -0500

    Update Rotate_String.java

--- Rotate_String.java
+++ Rotate_String.java
@@ -5,6 +5,18 @@ A shift on A consists of taking string A and moving the leftmost character to th
 
 class Solution {
     public boolean rotateString(String A, String B) {
+        int len = A.length();
         
+        if (A.length() != B.length()) return false;
+        if (A.equals(B)) return true;
+        
+        for (int i = 0; i < A.length(); i++) {
+            int k = i % len;
+            String s = A.substring(i) + A.substring(0, i);
+            
+            if (s.equals(B)) return true;
+        }
+        
+        return false;
     }
 }
