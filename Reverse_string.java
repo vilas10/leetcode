3cbc42f2f7447d3edbf6f71153e435798812817a
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:44:46 -0500

    Update Reverse_string.java

--- Reverse_string.java
+++ Reverse_string.java
@@ -2,8 +2,14 @@
 Write a function that takes a string as input and returns the string reversed.
 */
 
-class Solution {
+public class Solution {
     public String reverseString(String s) {
+        StringBuilder sb = new StringBuilder();
         
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        
+        return sb.toString();
     }
 }
