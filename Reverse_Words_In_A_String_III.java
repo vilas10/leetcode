142defd5dfa0c2985cf91fe3ded14604b630f2c0
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:43:32 -0500

    Update Reverse_Words_In_A_String_III.java

--- Reverse_Words_In_A_String_III.java
+++ Reverse_Words_In_A_String_III.java
@@ -2,8 +2,11 @@
 Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 */
 
-class Solution {
-    public String reverseWords(String s) {
-        
-    }
+object Solution {
+    def reverseWords(s: String): String = {
+        if (s == null)
+            null
+        else
+            s.trim.split("\\s+").map(word => word.reverse) mkString " "
+    }    
 }
