0f9cb54f6b7bdc1083419dd6ee6805ddebf0b949
vineverlas <vineverlas10@gmail.com>
2018-07-26 13:48:46 -0500

    Update Shortest_Distance_to_a_Character.java

--- Shortest_Distance_to_a_Character.java
+++ Shortest_Distance_to_a_Character.java
@@ -4,6 +4,38 @@ Given a string S and a character C, return an array of integers representing the
 
 class Solution {
     public int[] shortestToChar(String S, char C) {
+        int[] result = new int[S.length()];
+        int index = 0;
+        int i = 0;
         
+        int first = S.indexOf(C);
+        while (first >= 0) {
+            System.out.println("first: " + first);
+            result[i++] = first--;
+        }
+                
+        while (i < S.length()) {
+            int next = S.indexOf(C, i);
+            
+            int k = 1;
+            if (next == -1) {
+                while (i < S.length()) {    
+                    System.out.println("last k: " + k);
+
+                    result[i++] = k++; 
+                }
+            } else {
+                int curr = next;
+                while (i < curr) {
+                    System.out.println("k: " + k);
+                    result[i++] = k;
+                    result[--curr] = k;
+                    k++;
+                }
+                i = next + 1;
+            }
+        }
+        
+       return result;
     }
 }
