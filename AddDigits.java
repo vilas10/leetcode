bc59d360983cc60f64370ddc1404c844139c4f92
Vilas <vineverlas10@gmail.com>
2018-08-06 22:32:14 -0500

    Update and rename Add_Digits.java to AddDigits.java

--- AddDigits.java
+++ AddDigits.java
@@ -9,7 +9,7 @@ Follow up:
 Could you do it without any loop/recursion in O(1) runtime?
 */
 
-public class Add_Digits {
+public class AddDigits {
     /*public int addDigits(int num) {
         int sum = 0;
         
