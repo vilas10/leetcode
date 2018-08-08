c126f2e680bdaf5443094551a7fa896ab685b5ac
Vilas <vineverlas10@gmail.com>
2018-08-08 15:23:55 -0500

    Update and rename Distribute_Candies.java to DistributeCandies.java

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -2,7 +2,7 @@
 Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
 */
 
-class Solution {
+class DistributeCandies {
     public int distributeCandies(int[] candies) {
         Set<Integer> distinctCandies = new HashSet<Integer>();
         
