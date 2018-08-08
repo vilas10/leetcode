551ef52d04019113bd21721cf93c1134975bb333
Vilas <vineverlas10@gmail.com>
2018-08-08 15:41:28 -0500

    Update and rename Find_The_Duplicate_Number.Java to FindTheDuplicateNumber.java

--- FindTheDuplicateNumber.java
+++ FindTheDuplicateNumber.java
@@ -1,7 +1,7 @@
 /*
 Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
 */
-class Solution {
+class FindTheDuplicateNumber {
     public int findDuplicate(int[] nums) {
         BitSet bits = new BitSet(nums.length - 1);
         int i = 0;
