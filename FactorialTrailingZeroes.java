a8eb0ec6d77c1e7e19c273a263f016355c69cb42
Vilas <vineverlas10@gmail.com>
2018-08-08 15:28:35 -0500

    Update and rename Factorial_Trailing_Zeroes.java to FactorialTrailingZeroes.java

--- FactorialTrailingZeroes.java
+++ FactorialTrailingZeroes.java
@@ -4,7 +4,7 @@ Given an integer n, return the number of trailing zeroes in n!.
 Note: Your solution should be in logarithmic time complexity.
 */
 
-public class Solution {
+public class FactorialTrailingZeroes {
     public int trailingZeroes(int n) {
         if(n <= 0) {
             return 0;
