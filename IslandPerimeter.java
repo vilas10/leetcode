790d04c0ff17f1d951a606b72a8868fa6b334321
Vilas <vineverlas10@gmail.com>
2018-08-08 16:03:32 -0500

    Update and rename Island_perimeter.java to IslandPerimeter.java

--- IslandPerimeter.java
+++ IslandPerimeter.java
@@ -2,7 +2,7 @@
 You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 */
 
-class Solution {
+class IslandPerimeter {
     public int islandPerimeter(int[][] grid) {
         int perimeter = 0;
         for (int i = 0; i < grid.length; i++) {
