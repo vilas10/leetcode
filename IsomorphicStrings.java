e963d1eed69ca94e3d5549741f9a4f4c1ff231ad
Vilas <vineverlas10@gmail.com>
2018-08-08 16:04:33 -0500

    Update and rename Isomorphic_Strings.Java to IsomorphicStrings.java

--- IsomorphicStrings.java
+++ IsomorphicStrings.java
@@ -6,7 +6,7 @@ Two strings are isomorphic if the characters in s can be replaced to get t.
 All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
 */
 
-public class Solution {
+public class IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
         if((s == null && t == null) || (s.length() == 0 && t.length() == 0)) {
             return true;
