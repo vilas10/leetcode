64f1aef35ebba98508242bad8dcd53a7508ad2b6
Vilas <vineverlas10@gmail.com>
2018-08-08 16:05:13 -0500

    Update and rename Jewels_And_Stones.scala to JewelsAndStones.scala

--- JewelsAndStones.scala
+++ JewelsAndStones.scala
@@ -1,6 +1,6 @@
 /* https://leetcode.com/problems/jewels-and-stones/description/ */
 
-object Solution {
+object JewelsAndStones {
     def numJewelsInStones(J: String, S: String): Int = {
         val jewels = J.split("").toSet
         S.split("").filter(jewels(_)).size
