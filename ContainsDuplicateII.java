38746d57981186ba91d036cbabb6eb87dd57a5ad
Vilas <vineverlas10@gmail.com>
2018-08-08 15:07:01 -0500

    Update and rename Contains_Duplicate_II.java to ContainsDuplicateII.java

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -3,7 +3,7 @@ Given an array of integers and an integer k, find out whether there are
 two distinct indices i and j in the array such that nums[i] = nums[j] and 
 the difference between i and j is at most k.
 */
-public class Contains_Duplicate_II {
+public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         HashSet<Integer> set = new HashSet<Integer>();
         
