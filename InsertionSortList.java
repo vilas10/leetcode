cbae22787ec4e2899b8b50bc5aead77cc67fc0be
Vilas <vineverlas10@gmail.com>
2018-08-08 15:57:18 -0500

    Update and rename Insertion_Sort_List.java to InsertionSortList.java

--- InsertionSortList.java
+++ InsertionSortList.java
@@ -6,7 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
+public class InsertionSortList {
     public ListNode insertionSortList(ListNode head) {
         if(head == null || head.next == null) {
             return head;
