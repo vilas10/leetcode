6f75d608cb1a86c22f89990c778bb475f4486cc0
Vilas <vineverlas10@gmail.com>
2018-08-08 15:48:24 -0500

    Update and rename Hamming_Distance.java to HammingDistance.java

--- HammingDistance.java
+++ HammingDistance.java
@@ -3,7 +3,7 @@ The Hamming distance between two integers is the number of positions at which th
 Given two integers x and y, calculate the Hamming distance
 */
 
-class Solution {
+class HammingDistance {
     public int hammingDistance(int x, int y) {
         int ones = x ^ y;
         int count = 0;
