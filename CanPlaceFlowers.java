7bb14aee9d13adb3a05a00b9cf981ef2ef75d7e3
Vilas <vineverlas10@gmail.com>
2018-08-08 14:56:16 -0500

    Update and rename Can_Place_Flowers.Java to CanPlaceFlowers.java

--- CanPlaceFlowers.java
+++ CanPlaceFlowers.java
@@ -4,7 +4,7 @@ Suppose you have a long flowerbed in which some of the plots are planted and som
 Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
 */
 
-public class Solution {
+public class CanPlaceFlowers {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int i = 1;
 
