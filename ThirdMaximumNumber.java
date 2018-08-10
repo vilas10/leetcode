f8ac89f936a0da67c1e9955000182c86f676dc21
Vilas <vineverlas10@gmail.com>
2018-08-10 15:02:38 -0500

    Update and rename Third_Maximum_Number.Java to ThirdMaximumNumber.java

--- ThirdMaximumNumber.java
+++ ThirdMaximumNumber.java
@@ -1,7 +1,7 @@
 /*
 Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
 */
-class Solution {
+class ThirdMaximumNumber {
     public int thirdMax(int[] nums) {        
         Integer first = null;
         Integer second = null;
