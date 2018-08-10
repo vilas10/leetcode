160a7d3d8dffff108c535e40b88a48ffc706260e
Vilas <vineverlas10@gmail.com>
2018-08-10 14:17:52 -0500

    Update and rename Plus_One.java to PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -4,7 +4,7 @@ Given a non-negative number represented as an array of digits, plus one to the n
 The digits are stored such that the most significant digit is at the head of the list.
 */
 
-public class Solution {
+public class PlusOne {
     public int[] plusOne(int[] digits) {
         if(digits == null) {
             return null;
