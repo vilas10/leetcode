99163fbfeada8a4dc98e3463f739566eeaf28869
Vilas <vineverlas10@gmail.com>
2018-08-10 14:42:40 -0500

    Update and rename Roman_To_Integer.java to RomanToInteger.java

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -4,7 +4,7 @@ Given a roman numeral, convert it to an integer.
 Input is guaranteed to be within the range from 1 to 3999.
 */
 
-public class Solution {
+public class RomanToInteger {
     public int romanToInt(String s) {
         if(s == null || s.length() == 0) {
             return 0;
