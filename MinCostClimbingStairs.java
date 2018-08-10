bf1516c0bc83c9ac85440f097f0452cce377c465
Vilas <vineverlas10@gmail.com>
2018-08-10 13:56:56 -0500

    Update and rename Min_Cost_Climbing_Stairs.Java to MinCostClimbingStairs.java

--- MinCostClimbingStairs.java
+++ MinCostClimbingStairs.java
@@ -4,7 +4,7 @@ On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 ind
 Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
 */
 
-class Solution {
+class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
         if (cost.length == 2) return Math.min(cost[0], cost[1]);
         
