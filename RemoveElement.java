15e270a84e8cf486723fc0dac9b9c8f93a26293b
Vilas <vineverlas10@gmail.com>
2018-08-10 14:31:47 -0500

    Update and rename Remove_Element.java to RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -4,7 +4,7 @@ Given an array and a value, remove all instances of that value in place and retu
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 */
 
-public class Solution {
+public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         if(nums == null || nums.length == 0) {
             return 0;
