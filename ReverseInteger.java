ecd16fc53fc8c81371d32fba47825b4a970cd9f3
Vilas <vineverlas10@gmail.com>
2018-08-10 14:36:55 -0500

    Update and rename Reverse_Integer.java to ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -5,7 +5,7 @@ Example1: x = 123, return 321
 Example2: x = -123, return -321
 */
 
-public class Solution {
+public class ReverseInteger {
     public int reverse(int x) {
         
         int flag=0;
