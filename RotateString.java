07c44c94fc0619def7be1459d5979ceb6daf2d99
Vilas <vineverlas10@gmail.com>
2018-08-10 14:44:22 -0500

    Update and rename Rotate_String.java to RotateString.java

--- RotateString.java
+++ RotateString.java
@@ -3,7 +3,7 @@ We are given two strings, A and B.
 A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
 */
 
-class Solution {
+class RotateString {
     public boolean rotateString(String A, String B) {
         int len = A.length();
         
