1e730722726a5fb731fa9d59e7ebaa871dfd4889
Vilas <vineverlas10@gmail.com>
2018-08-10 14:54:26 -0500

    Update and rename Sqrt(x).Java to Sqrt(x).java

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -5,7 +5,7 @@ Compute and return the square root of x, where x is guaranteed to be a non-negat
 
 Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
 */
-public class Solution {
+public class Sqrt(x) {
     public int mySqrt(int x) {
         
         if(x == 0 || x == 1) {
