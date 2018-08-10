85aeb5fc56ed384571452bf6e001e42706eccb1b
Vilas <vineverlas10@gmail.com>
2018-08-10 15:08:19 -0500

    Update and rename Two_Sum_II - Input_array_is_sorted.Java to TwoSumIIInputArrayIsSorted.java

--- TwoSumIIInputArrayIsSorted.java
+++ TwoSumIIInputArrayIsSorted.java
@@ -2,7 +2,7 @@
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
 */
-class Solution {
+class TwoSumIIInputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
         int i1 = 0, i2 = 0;
