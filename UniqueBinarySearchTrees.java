6923698d8caabe94cf6c20e87955d0fcd01816c5
Vilas <vineverlas10@gmail.com>
2018-08-10 15:10:10 -0500

    Update and rename Unique_Binary_Search_Trees.Java to UniqueBinarySearchTrees.java

--- UniqueBinarySearchTrees.java
+++ UniqueBinarySearchTrees.java
@@ -1,7 +1,7 @@
 /*
 Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
 */
-public class Solution {
+public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
         
         int[] dp = new int[n+1];
