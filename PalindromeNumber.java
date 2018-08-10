fd3c04efd8394684c2574d4957ac675aa09fcad1
Vilas <vineverlas10@gmail.com>
2018-08-10 14:08:46 -0500

    Update and rename Palindrome_Number.Java to PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,7 +2,7 @@
 Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 */
 
-public class Solution {
+public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         if(x < 0) {
             return false;
